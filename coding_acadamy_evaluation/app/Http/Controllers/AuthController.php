d0996c31516bf09948272ab113df2ce221c50165
SAGBO4 <michael.sagbo@epitech.eu>
2025-10-16 16:37:46 +0100

    feat: init and creation of auth controller and all migration

--- coding_acadamy_evaluation/app/Http/Controllers/AuthController.php
+++ coding_acadamy_evaluation/app/Http/Controllers/AuthController.php
@@ -1,10 +1,26 @@
 <?php
 
+// app/Http/Controllers/AuthController.php
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        return response()->json(['message' => 'Utilisateur créé', 'user' => $user], 201);
+    }
 }
